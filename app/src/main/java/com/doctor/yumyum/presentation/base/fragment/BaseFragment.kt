a2d3406a507449aca5b63e8b52ea66458c880469
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-10-12 00:19:38 +0900

    [Feat] Base 생성

---
+++ app/src/main/java/com/doctor/yumyum/presentation/base/fragment/BaseFragment.kt
@@ -0,0 +1,33 @@
+package com.doctor.yumyum.presentation.base.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+    protected lateinit var binding: T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.lifecycleOwner = this
+    }
+
+    protected fun showToast(message: String) =
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+
+}
---
+++ app/src/main/java/com/doctor/yumyum/presentation/base/viewmodel/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.doctor.yumyum.presentation.base.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val job = SupervisorJob()
+    protected val uiScope = CoroutineScope(Dispatchers.Main + job)
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+}
