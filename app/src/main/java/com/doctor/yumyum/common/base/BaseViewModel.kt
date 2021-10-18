962eae4dd9458611fdc98230ff3879ba465791de
jionchu <fn0809@gmail.com>
2021-10-18 17:15:39 +0900

    [Refactor] BaseViewModel 수정
    - 불필요한 uiScope 삭제
    - baseState 설정

--- app/src/main/java/com/doctor/yumyum/common/base/BaseViewModel.kt
+++ app/src/main/java/com/doctor/yumyum/common/base/BaseViewModel.kt
@@ -1,17 +1,24 @@
 package com.doctor.yumyum.common.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel : ViewModel() {
 
-    private val job = SupervisorJob()
-    protected val uiScope = CoroutineScope(Dispatchers.Main + job)
+    // toast message 띄울 때 사용
+    private val _baseState = MutableLiveData<BaseState>()
+    val baseState
+        get() = _baseState
 
-    override fun onCleared() {
-        job.cancel()
-        super.onCleared()
+    sealed class BaseState {
+        data class Toast(val string: String): BaseState()
+    }
+
+    private fun toast(message: String) {
+        viewModelScope.launch {
+            _baseState.value = BaseState.Toast(message)
+        }
     }
 }
