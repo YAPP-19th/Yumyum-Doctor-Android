0a34f976cc6608846fdbe3224fc7fd3dfc73acdd
jionchu <fn0809@gmail.com>
2021-10-30 12:43:08 +0900

    [Fix] ViewModel 생성할 때 ViewModelProvider, ViewModelFactory 사용하도록 변경
    - ViewModelProvider 사용하지 않으면 일반 클래스와 같아서 view의 lifecycle을 알 수 없음

--- app/src/main/java/com/doctor/yumyum/presentation/ui/main/ResearchRecipeFragment.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/main/ResearchRecipeFragment.kt
@@ -5,40 +5,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseFragment
 import com.doctor.yumyum.databinding.FragmentResearchRecipeBinding
+import com.doctor.yumyum.presentation.ResearchViewModelFactory
 import com.doctor.yumyum.presentation.viewmodel.ResearchRecipeViewModel
 
 class ResearchRecipeFragment :
     BaseFragment<FragmentResearchRecipeBinding>(R.layout.fragment_research_recipe) {
 
-    private lateinit var viewModel: ResearchRecipeViewModel
+    // TODO: sharedpreferences object 불러오기
+    private val sharedPreferences by lazy {
+        context?.getSharedPreferences(
+            getString(R.string.shared_pref_key),
+            Application.MODE_PRIVATE
+        )
+    }
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            ResearchViewModelFactory(sharedPreferences!!)
+        )[ResearchRecipeViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // TODO: sharedpreferences object 불러오기
-        val sharedPreferences =
-            context?.getSharedPreferences(
-                getString(R.string.shared_pref_key),
-                Application.MODE_PRIVATE
-            )
-        viewModel = ResearchRecipeViewModel(sharedPreferences!!)
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
 
         viewModel.mode.observe(viewLifecycleOwner) {
             binding.researchRecipeIbMode.setImageResource(
-                if (it == R.string.food) R.string.food else R.drawable.ic_change_food
+                if (it == R.string.food) R.drawable.ic_change_food else R.drawable.ic_change_beverage
             )
         }
         binding.viewModel = viewModel
+        return binding.root
     }
-
 }
