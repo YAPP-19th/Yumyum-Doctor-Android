492e302aa018a80c690124c3138869ef1a5ff1d7
jionchu <fn0809@gmail.com>
2021-10-30 11:55:29 +0900

    [Refactor] SharedPreferences ViewModel에서 사용하도록 수정

--- app/src/main/java/com/doctor/yumyum/presentation/ui/main/ResearchRecipeFragment.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/main/ResearchRecipeFragment.kt
@@ -1,10 +1,10 @@
 package com.doctor.yumyum.presentation.ui.main
 
+import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseFragment
 import com.doctor.yumyum.databinding.FragmentResearchRecipeBinding
@@ -24,14 +24,19 @@ class ResearchRecipeFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO: sharedpreferences에서 현재 모드 가져오기
-        viewModel = ResearchRecipeViewModel("음식")
+
+        // TODO: sharedpreferences object 불러오기
+        val sharedPreferences =
+            context?.getSharedPreferences(
+                getString(R.string.shared_pref_key),
+                Application.MODE_PRIVATE
+            )
+        viewModel = ResearchRecipeViewModel(sharedPreferences!!)
+
         viewModel.mode.observe(viewLifecycleOwner) {
-            if (it.equals("음식")) {
-                binding.researchRecipeIbMode.setImageResource(R.drawable.ic_change_beverage)
-            } else {
-                binding.researchRecipeIbMode.setImageResource(R.drawable.ic_change_food)
-            }
+            binding.researchRecipeIbMode.setImageResource(
+                if (it == R.string.food) R.string.food else R.drawable.ic_change_food
+            )
         }
         binding.viewModel = viewModel
     }
--- app/src/main/java/com/doctor/yumyum/presentation/viewmodel/ResearchRecipeViewModel.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/viewmodel/ResearchRecipeViewModel.kt
@@ -1,20 +1,27 @@
 package com.doctor.yumyum.presentation.viewmodel
 
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseViewModel
 
-class ResearchRecipeViewModel(currentMode: String) : BaseViewModel() {
-    private val _mode: MutableLiveData<String> = MutableLiveData(currentMode)
-    val mode: LiveData<String>
+class ResearchRecipeViewModel(private val sharedPreferences: SharedPreferences) : BaseViewModel() {
+    @SuppressLint("SupportAnnotationUsage")
+    @StringRes
+    private val _mode: MutableLiveData<Int> =
+        MutableLiveData(sharedPreferences.getInt("mode", R.string.food))
+    val mode: LiveData<Int>
         get() = _mode
 
     fun changeMode() {
-        if (mode.value.equals("음식")) {
-            _mode.value = "음료"
-        } else {
-            _mode.value = "음식"
-        }
-        // TODO: sharedpreferences에 현재 모드 저장
+        _mode.value = if (mode.value == R.string.food) R.string.beverage else R.string.food
+
+        // 현재 모드 저장
+        val editor = sharedPreferences.edit()
+        editor.putInt("mode", mode.value ?: R.string.food)
+        editor.apply()
     }
 }
