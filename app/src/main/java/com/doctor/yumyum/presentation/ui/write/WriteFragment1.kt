95777ec9d6318cf0fe2e2c67123458e248f099b1
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-10-31 23:08:48 +0900

    [Feat] 브랜드 선택 다이얼로그

--- app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteFragment1.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteFragment1.kt
@@ -1,8 +1,43 @@
 package com.doctor.yumyum.presentation.ui.write
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseFragment
 import com.doctor.yumyum.databinding.FragmentWriteFirstBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class WriteFragment1 : BaseFragment<FragmentWriteFirstBinding>(R.layout.fragment_write_first) {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomSheetDialog : BottomSheetDialog
+    private lateinit var bottomSheetView : View
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initBinding()
+        initDialog()
+
+        navController = Navigation.findNavController(view)
+        binding.writeFirstBtnNext.setOnClickListener {
+            navController.navigate(R.id.action_first_write_fragment_to_second_write_fragment)
+        }
+    }
+
+    private fun initBinding() {
+        binding.firstFragment = this
+    }
+
+    private fun initDialog() {
+        bottomSheetView = layoutInflater.inflate(R.layout.dialog_select_brand, null)
+        bottomSheetDialog = BottomSheetDialog(requireContext())
+        bottomSheetDialog.setContentView(bottomSheetView)
+    }
+
+    fun showBottomSheet(){
+        bottomSheetDialog.show()
+    }
 }
