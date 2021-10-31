5ae27e2985d73f202366515e445a74dce21480b5
junieberry <junyoung44@g.skku.edu>
2021-11-01 01:54:08 +0900

    [Feat] 카카오 SDK 초기화

--- app/src/main/java/com/doctor/yumyum/presentation/ui/login/LoginActivity.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/login/LoginActivity.kt
@@ -5,6 +5,14 @@ import android.os.Bundle
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseActivity
 import com.doctor.yumyum.databinding.ActivityLoginBinding
+import android.content.pm.PackageManager
+
+import android.content.pm.PackageInfo
+import android.util.Base64
+import android.util.Log
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,4 +23,5 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
             lifecycleOwner = this@LoginActivity
         }
     }
+
 }
