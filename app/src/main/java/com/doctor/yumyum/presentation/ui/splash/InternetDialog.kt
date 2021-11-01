613b1ef7b6a3b6312b29cdef823aaf4546757472
junieberry <junyoung44@g.skku.edu>
2021-11-02 03:01:01 +0900

    [Feat] 로그인 Error Dialog 추가

--- app/src/main/java/com/doctor/yumyum/presentation/ui/splash/InternetDialog.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/splash/InternetDialog.kt
@@ -1,17 +1,14 @@
 package com.doctor.yumyum.presentation.ui.splash
 
-import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getSystemService
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseDialog
 import com.doctor.yumyum.databinding.DialogInternetBinding
-import com.doctor.yumyum.presentation.ui.main.MainActivity
+import com.doctor.yumyum.presentation.ui.login.LoginActivity
 
 class InternetDialog : BaseDialog<DialogInternetBinding>(R.layout.dialog_internet) {
     override fun onCreateView(
@@ -21,8 +18,8 @@ class InternetDialog : BaseDialog<DialogInternetBinding>(R.layout.dialog_interne
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         binding.internetBtn.setOnClickListener {
-            val splashIntent = Intent(this.context, SplashActivity::class.java)
-            startActivity(splashIntent)
+            val loginIntent = Intent(this.context, LoginActivity::class.java)
+            startActivity(loginIntent)
         }
         return binding.root
     }
