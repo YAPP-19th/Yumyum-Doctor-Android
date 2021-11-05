86c272207bfc9a63f948994c06226b15ae1d896b
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-11-05 23:12:55 +0900

    [Feat] Toolbar 버튼 클릭

--- app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteRecipeActivity.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteRecipeActivity.kt
@@ -1,13 +1,35 @@
 package com.doctor.yumyum.presentation.ui.write
 
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseActivity
 import com.doctor.yumyum.databinding.ActivityWriteRecipeBinding
 import com.doctor.yumyum.presentation.viewmodel.WriteRecipeViewModel
+import com.google.android.material.navigation.NavigationView
 
-class WriteRecipeActivity : BaseActivity<ActivityWriteRecipeBinding>(R.layout.activity_write_recipe){
+class WriteRecipeActivity : BaseActivity<ActivityWriteRecipeBinding>(R.layout.activity_write_recipe), View.OnClickListener{
 
-    private lateinit var viewModel: WriteRecipeViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.writeIbBack.setOnClickListener(this)
+        binding.writeTvCancel.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.writeIbBack ->
+                //TODO :: 첫번째 작성하기에서는 뒤로가기 안보이게 하기
+                findNavController(R.id.nav_host).popBackStack()
+            binding.writeTvCancel ->
+                //TODO :: 다이얼로그 띄우기
+                finish()
+        }
+    }
 
 }
