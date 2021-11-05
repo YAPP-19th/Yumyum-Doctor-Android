6c8b0339397225d60aaf29537152a8604fba0f56
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-11-06 02:10:39 +0900

    [Feat] startForResult 적용

--- app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteFragment2.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteFragment2.kt
@@ -1,10 +1,10 @@
 package com.doctor.yumyum.presentation.ui.write
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.NavController
-import androidx.navigation.NavDirections
-import androidx.navigation.Navigation
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseFragment
@@ -15,12 +15,21 @@ import com.doctor.yumyum.databinding.FragmentWriteSecondBinding
  *  - 레시피 이름, 메인메뉴 입력하기
  *  - 추가하는 재료 & 빼는 재료 보여주기
  */
+
 class WriteFragment2 : BaseFragment<FragmentWriteSecondBinding>(R.layout.fragment_write_second), View.OnClickListener{
+    private lateinit var changeIngredients : ActivityResultLauncher<Intent>
+
+    companion object {
+        private const val REQUEST_CODE_ADD_INGREDIENTS = 9001
+        private const val REQUEST_CODE_MINUS_INGREDIENTS = 9002
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initBinding()
+        changeIngredients()
 
         binding.writeSecondBtnMinus.setOnClickListener(this)
         binding.writeSecondBtnNext.setOnClickListener(this)
@@ -32,16 +41,35 @@ class WriteFragment2 : BaseFragment<FragmentWriteSecondBinding>(R.layout.fragmen
     }
 
     override fun onClick(v: View?) {
+        val intent = Intent(context,WriteTagActivity::class.java)
         when(v){
-            binding.writeSecondBtnAdd ->
-                findNavController().navigate(R.id.action_second_write_fragment_to_tag_write_fragment)
+            binding.writeSecondBtnAdd -> {
+                intent.putExtra(resources.getString(R.string.write_tag_type), REQUEST_CODE_ADD_INGREDIENTS)
+                changeIngredients.launch(intent)
+            }
 
             binding.writeSecondBtnNext->
                 findNavController().navigate(R.id.action_second_write_fragment_to_third_write_fragment)
 
-            binding.writeSecondBtnMinus->
-                findNavController().navigate(R.id.action_second_write_fragment_to_tag_write_fragment)
+            binding.writeSecondBtnMinus-> {
+                intent.putExtra(resources.getString(R.string.write_tag_type), REQUEST_CODE_MINUS_INGREDIENTS)
+                changeIngredients.launch(intent)
+            }
+        }
+    }
 
+    //TagActivity가 종료되었을 때
+    private fun changeIngredients() {
+        changeIngredients = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == REQUEST_CODE_ADD_INGREDIENTS) {
+                //TODO :: Recycler 아이템 업데이트
+                showToast("추가하는 재료 입력 완료")
+            }
+
+            if(it.resultCode == REQUEST_CODE_MINUS_INGREDIENTS){
+                //TODO :: Recycler 아이템 업데이트
+                showToast("빼거나 수정하는 재료 입력 완료")
+            }
         }
     }
 }
