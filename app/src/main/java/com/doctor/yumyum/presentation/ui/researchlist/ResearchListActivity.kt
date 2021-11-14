7a03f747e1e935e0d1b6ae7645d04a9a8360fb5a
jionchu <fn0809@gmail.com>
2021-11-14 12:31:00 +0900

    [Feat] 정렬 다이얼로그 선택에 따른 디자인 변경 추가

--- app/src/main/java/com/doctor/yumyum/presentation/ui/researchlist/ResearchListActivity.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/researchlist/ResearchListActivity.kt
@@ -2,10 +2,13 @@ package com.doctor.yumyum.presentation.ui.researchlist
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseActivity
 import com.doctor.yumyum.databinding.ActivityResearchListBinding
+import com.doctor.yumyum.databinding.DialogSelectSortBinding
 import com.doctor.yumyum.presentation.ui.filter.FilterActivity
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
@@ -37,15 +40,25 @@ class ResearchListActivity :
         }
         // 정렬 다이얼로그 띄우기
         binding.researchListTvSort.setOnClickListener { showBottomSheet() }
+        viewModel.sortType.observe(this) { bottomSheetDialog.dismiss() }
     }
 
     private fun initDialog() {
         val bottomSheetView = layoutInflater.inflate(R.layout.dialog_select_sort, null)
+        val bottomSheetBinding = DataBindingUtil.inflate<DialogSelectSortBinding>(
+            layoutInflater,
+            R.layout.dialog_select_sort,
+            bottomSheetView as ViewGroup,
+            false
+        )
+        bottomSheetBinding.lifecycleOwner = this
+        bottomSheetBinding.viewModel = viewModel
         bottomSheetDialog = BottomSheetDialog(this)
-        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.setContentView(bottomSheetBinding.root)
     }
 
     private fun showBottomSheet() {
         bottomSheetDialog.show()
+        viewModel.initSortType()
     }
 }
