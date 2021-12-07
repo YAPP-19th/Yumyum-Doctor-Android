1f24f91131f7f57df2d3d5830e96b0bff61a0e41
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-12-07 20:07:19 +0900

    [Add] 입력값 최대 10개

--- app/src/main/java/com/doctor/yumyum/presentation/ui/write/viewmodel/WriteTagViewModel.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/write/viewmodel/WriteTagViewModel.kt
@@ -24,7 +24,7 @@ class WriteTagViewModel : BaseViewModel(){
     }
 
     private fun addTagItem(newTag : String){
-        if(!_tagListLiveData.value?.contains(newTag)!!){
+        if(!_tagListLiveData.value?.contains(newTag)!! && _tagListLiveData.value?.size != 10){
             _tagListLiveData.value?.add(newTag)
             _tagListLiveData.value = _tagListLiveData.value
         }
