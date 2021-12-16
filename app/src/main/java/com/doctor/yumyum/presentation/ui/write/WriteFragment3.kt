2294833348bdc589da591958d8c38f03dcc253dc
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-12-16 23:22:47 +0900

    [Design] 레시피 작성하기4 화면 구성

--- app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteFragment3.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteFragment3.kt
@@ -1,5 +1,8 @@
 package com.doctor.yumyum.presentation.ui.write
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseFragment
 import com.doctor.yumyum.databinding.FragmentWriteThirdBinding
@@ -11,4 +14,12 @@ import com.doctor.yumyum.databinding.FragmentWriteThirdBinding
  */
 
 class WriteFragment3 : BaseFragment<FragmentWriteThirdBinding>(R.layout.fragment_write_third){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.writeThirdBtnNext.setOnClickListener{
+            findNavController().navigate(R.id.action_third_write_fragment_to_fourth_write_fragment)
+        }
+    }
 }
