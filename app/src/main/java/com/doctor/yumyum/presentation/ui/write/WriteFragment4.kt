a3a4d83f25215857dfd507c2c83d770ee819851b
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-12-18 21:01:37 +0900

    [Feat] 다음버튼 리스너

--- app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteFragment4.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/write/WriteFragment4.kt
@@ -1,5 +1,8 @@
 package com.doctor.yumyum.presentation.ui.write
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseFragment
 import com.doctor.yumyum.databinding.FragmentWriteFourthBinding
@@ -9,4 +12,13 @@ import com.doctor.yumyum.databinding.FragmentWriteFourthBinding
  * - 맛과 세부맛 선택하기
  */
 class WriteFragment4 : BaseFragment<FragmentWriteFourthBinding>(R.layout.fragment_write_fourth) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.writeFourthBtnNext.setOnClickListener {
+            findNavController().navigate(R.id.action_fourth_write_fragment_to_fifth_write_fragment)
+        }
+    }
+
 }
