0e4f6de9628ad862591c7a956b3847a5fa5485d3
jionchu <fn0809@gmail.com>
2021-12-20 22:51:45 +0900

    [Feat] 레시피 상세화면 스크롤뷰 설정

--- app/src/main/java/com/doctor/yumyum/presentation/ui/recipedetail/RecipeDetailActivity.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/recipedetail/RecipeDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.doctor.yumyum.presentation.ui.recipedetail
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.lifecycle.ViewModelProvider
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseActivity
@@ -25,6 +26,12 @@ class RecipeDetailActivity :
         super.onCreate(savedInstanceState)
 
         val recipeId = intent.extras?.getInt("recipeId") ?: 1
+
+        val displayMetrics = DisplayMetrics()
+        this.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
+        val deviceWidth = (displayMetrics.widthPixels * 0.8).toInt()
+        binding.recipeDetailIvRecipe.layoutParams.height = deviceWidth
+        //TODO: content layout margin top 설정
 
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
