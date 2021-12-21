f34100ee005bc7dde45d78d6692d366fbd4c0d9d
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-12-22 03:17:01 +0900

    [Feat] 나의레시피 FilterActivity 생성

--- app/src/main/java/com/doctor/yumyum/presentation/ui/myrecipe/MyRecipeFragment.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/myrecipe/MyRecipeFragment.kt
@@ -1,24 +1,29 @@
 package com.doctor.yumyum.presentation.ui.myrecipe
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseFragment
 import com.doctor.yumyum.databinding.FragmentMyRecipeBinding
+import com.doctor.yumyum.presentation.ui.write.WriteTagActivity
 
 
 class MyRecipeFragment : BaseFragment<FragmentMyRecipeBinding>(R.layout.fragment_my_recipe) {
-
+    private lateinit var sortLauncher: ActivityResultLauncher<Intent>
     private val myRecipeViewModel : MyRecipeViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
+        startForSort()
         binding.lifecycleOwner = this
         binding.viewModel = myRecipeViewModel
 
@@ -26,6 +31,16 @@ class MyRecipeFragment : BaseFragment<FragmentMyRecipeBinding>(R.layout.fragment
             binding.myRecipeIbMode.setImageResource(
                 if (it == R.string.common_food) R.drawable.ic_change_food else R.drawable.ic_change_beverage
             )
+        }
+    }
+
+    private fun startForSort() {
+        sortLauncher= registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            //TODO : 정렬 정보 가져오기
+        }
+        binding.myRecipeIbSort.setOnClickListener {
+            val intent = Intent(context, MyPageFilterActivity::class.java)
+            sortLauncher.launch(intent)
         }
     }
 }
