0bb234f275783fc5448dd233e4c645fe4f5a0259
junieberry <junyoung44@g.skku.edu>
2021-12-22 14:52:59 +0900

    [Feat] Home 화면 레이아웃

--- app/src/main/java/com/doctor/yumyum/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/home/HomeViewModel.kt
@@ -1,6 +1,22 @@
 package com.doctor.yumyum.presentation.ui.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseViewModel
+import com.doctor.yumyum.data.repository.MainRepositoryImpl
 
 class HomeViewModel : BaseViewModel() {
+    private val mainRepository = MainRepositoryImpl()
+
+    private val _mode: MutableLiveData<Int> = MutableLiveData(mainRepository.getMode())
+    val mode: LiveData<Int>
+        get() = _mode
+
+    fun changeMode() {
+        _mode.value =
+            if (mode.value == R.string.common_food) R.string.common_beverage else R.string.common_food
+
+        mainRepository.setMode(mode.value ?: R.string.common_food)
+    }
 }
