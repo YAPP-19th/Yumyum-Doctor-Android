098d4b6dca088f385712d81ea0102731cb7f4f2d
jionchu <fn0809@gmail.com>
2021-12-25 13:50:14 +0900

    [Feat] 레시피 목록 recycler view 설정

---
+++ app/src/main/java/com/doctor/yumyum/presentation/adapter/ResearchListAdapter.kt
@@ -0,0 +1,51 @@
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.doctor.yumyum.data.model.RankRecipe
+import com.doctor.yumyum.databinding.ItemResearchRecipeBinding
+
+class ResearchListAdapter(private val itemClickListener: (Int) -> Unit) :
+    RecyclerView.Adapter<ResearchListAdapter.ViewHolder>() {
+    private val recipeList: ArrayList<RankRecipe> = arrayListOf()
+
+    inner class ViewHolder(private val binding: ItemResearchRecipeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(recipe: RankRecipe) {
+            binding.itemResearchRecipeTvBrand.text = recipe.categoryName
+            binding.itemResearchRecipeTvTitle.text = recipe.foodName
+            binding.itemResearchRecipeTvCost.text = "${recipe.price}원"
+            binding.root.setOnClickListener { itemClickListener(recipe.id) }
+
+            if (recipe.foodImages.size > 0) {
+                Glide
+                    .with(binding.itemResearchRecipeIvImage)
+                    .load(recipe.foodImages[0].imageUrl)
+                    .into(binding.itemResearchRecipeIvImage)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemResearchRecipeBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(recipeList[position])
+    }
+
+    override fun getItemCount(): Int = recipeList.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setRecipeList(newRecipeList: ArrayList<RankRecipe>) {
+        recipeList.clear()
+        recipeList.addAll(newRecipeList)
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/doctor/yumyum/presentation/ui/researchlist/ResearchListActivity.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/researchlist/ResearchListActivity.kt
@@ -1,5 +1,6 @@
 package com.doctor.yumyum.presentation.ui.researchlist
 
+import ResearchListAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import com.doctor.yumyum.common.base.BaseActivity
 import com.doctor.yumyum.databinding.ActivityResearchListBinding
 import com.doctor.yumyum.databinding.DialogSelectSortBinding
 import com.doctor.yumyum.presentation.ui.filter.FilterActivity
+import com.doctor.yumyum.presentation.ui.recipedetail.RecipeDetailActivity
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.coroutines.launch
 
@@ -38,9 +40,31 @@ class ResearchListActivity :
         binding.researchListTvFilter.setOnClickListener {
             startActivity(Intent(this, FilterActivity::class.java))
         }
+        val researchListAdapter = ResearchListAdapter { recipeId ->
+            val intent = Intent(this, RecipeDetailActivity::class.java)
+            intent.putExtra("recipeId", recipeId)
+            startActivity(intent)
+        }
+        binding.researchListRvRecipe.adapter = researchListAdapter
+
         viewModel.sortType.observe(this) { bottomSheetDialog.dismiss() }
         lifecycleScope.launch {
-            viewModel.searchRecipeList(categoryName, "", "", 0, 100000, "like", "asc", "2021-12-26T12:12:12", 0, 10)
+            viewModel.searchRecipeList(
+                categoryName,
+                "",
+                "",
+                0,
+                100000,
+                "like",
+                "asc",
+                "2021-12-26T12:12:12",
+                0,
+                10
+            )
+        }
+
+        viewModel.recipeList.observe(this) {
+            researchListAdapter.setRecipeList(it)
         }
     }
 
