f3455ae19552f206a9353b65c240477dc6013c08
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-01-03 23:26:44 +0900

    [Feat] 최애레시피 데이터바인딩

--- app/src/main/java/com/doctor/yumyum/presentation/adapter/MyRecipeFavoriteAdapter.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/adapter/MyRecipeFavoriteAdapter.kt
@@ -5,20 +5,43 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.doctor.yumyum.R
 import com.doctor.yumyum.data.model.FavoriteRecipe
 import com.doctor.yumyum.data.remote.response.FavoriteRecipeResponse
 import com.doctor.yumyum.databinding.DialogWriteBinding
 import com.doctor.yumyum.databinding.ItemMyrecipeFavoriteBinding
 import com.doctor.yumyum.databinding.ItemSearchTasteBinding
 
-class MyRecipeFavoriteAdapter(private val itemClickListener: (Int) -> Unit) :
+class MyRecipeFavoriteAdapter(
+    private val itemClickListener: (Int) -> Unit,
+    private val deleteFavorite: (Int) -> Unit
+) :
     RecyclerView.Adapter<MyRecipeFavoriteAdapter.ViewHolder>() {
     private val favoriteList: ArrayList<FavoriteRecipe> = arrayListOf()
 
     inner class ViewHolder(private val binding: ItemMyrecipeFavoriteBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(recipe: FavoriteRecipe) {
-                binding.
+            binding.itemMyFavoriteTvBrand.text = recipe.categoryName
+            binding.itemMyFavoriteTvPrice.text =
+                binding.root.resources.getString(R.string.common_variable_price, recipe.price)
+            binding.itemMyFavoriteTvTitle.text = recipe.foodTitle
+
+            if (recipe.meFavorite) {
+                binding.itemMyFavoriteIbHeart.setImageResource(R.drawable.ic_heart_selected)
+            } else {
+                binding.itemMyFavoriteIbHeart.setImageResource(R.drawable.ic_heart_unselected)
+            }
+
+            if (recipe.foodImages.isNotEmpty()) {
+                Glide
+                    .with(binding.itemMyFavoriteIvImage)
+                    .load(recipe.foodImages[0].imageUrl)
+                    .placeholder(R.drawable.ic_loading_image)
+                    .into(binding.itemMyFavoriteIvImage)
+            }
+
         }
 
     }
