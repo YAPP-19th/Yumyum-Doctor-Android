e335b3b214548e800d371aaed57ff8ea1c91e089
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-01-04 20:39:18 +0900

    [Update] enum 값으로 변경

--- app/src/main/java/com/doctor/yumyum/common/utils/CommonType.kt
+++ app/src/main/java/com/doctor/yumyum/common/utils/CommonType.kt
@@ -4,3 +4,11 @@ package com.doctor.yumyum.common.utils
 enum class RecipeType {
     BASIC,MYFOOD,BOOKMARK
 }
+
+enum class StatusType {
+    MINE,SHARED
+}
+
+enum class TagType {
+    MAIN,ADD,EXTRACT
+}
