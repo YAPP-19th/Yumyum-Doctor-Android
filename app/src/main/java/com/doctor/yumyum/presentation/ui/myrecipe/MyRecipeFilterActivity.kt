db072cc9304252e4524a76e5cf490d4cbe525a52
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-01-04 23:54:35 +0900

    [Refactor] 클래스 이름 변경

--- app/src/main/java/com/doctor/yumyum/presentation/ui/myrecipe/MyRecipeFilterActivity.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/myrecipe/MyRecipeFilterActivity.kt
@@ -1,17 +1,24 @@
 package com.doctor.yumyum.presentation.ui.myrecipe
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseActivity
 import com.doctor.yumyum.databinding.ActivityMyRecipeFilterBinding
+import com.doctor.yumyum.presentation.ui.myrecipe.viewmodel.MyRecipeFilterViewModel
+import com.doctor.yumyum.presentation.ui.myrecipe.viewmodel.MyRecipeViewModel
 
-class MyPageFilterActivity :
+class MyRecipeFilterActivity :
     BaseActivity<ActivityMyRecipeFilterBinding>(R.layout.activity_my_recipe_filter) {
+    private val filterViewModel : MyRecipeFilterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.lifecycleOwner = this
         binding.fragment = this
+        binding.viewModel = filterViewModel
         binding.writeToolbar.appbarIbBack.setOnClickListener { finish() }
     }
 }
--- app/src/main/java/com/doctor/yumyum/presentation/ui/myrecipe/viewmodel/MyPageFilterViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.doctor.yumyum.presentation.ui.myrecipe.viewmodel
-
-import com.doctor.yumyum.common.base.BaseViewModel
-
-class MyPageFilterViewModel : BaseViewModel() {
-
-}
