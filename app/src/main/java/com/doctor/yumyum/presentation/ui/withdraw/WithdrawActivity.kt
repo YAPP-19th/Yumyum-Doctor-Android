e898a3094127159d7182c02daccd19c67ce7d24b
junieberry <junyoung44@g.skku.edu>
2022-01-04 16:28:50 +0900

    [Feat] 회원 탈퇴 사유 선택

--- app/src/main/java/com/doctor/yumyum/presentation/ui/withdraw/WithdrawActivity.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/withdraw/WithdrawActivity.kt
@@ -1,15 +1,23 @@
 package com.doctor.yumyum.presentation.ui.withdraw
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseActivity
 import com.doctor.yumyum.databinding.ActivityWithdrawBinding
 
 class WithdrawActivity : BaseActivity<ActivityWithdrawBinding>(R.layout.activity_withdraw) {
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        )[WithdrawViewModel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding.viewModel = viewModel
         binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/doctor/yumyum/presentation/ui/withdraw/WithdrawViewModel.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/withdraw/WithdrawViewModel.kt
@@ -1,6 +1,22 @@
 package com.doctor.yumyum.presentation.ui.withdraw
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.doctor.yumyum.common.base.BaseViewModel
 
 class WithdrawViewModel : BaseViewModel() {
+    private val _withdrawType: MutableLiveData<Int> = MutableLiveData(REASON_RARELY)
+    val withdrawType: LiveData<Int> get() = _withdrawType
+
+    fun setWithdrawType(type: Int) {
+        _withdrawType.value = type
+    }
+
+    companion object {
+        const val REASON_RARELY = 1
+        const val REASON_UNCOMFORTABLE = 2
+        const val REASON_UNPLEASANT = 3
+        const val REASON_WRONG = 4
+        const val REASON_ETC = 5
+    }
 }
