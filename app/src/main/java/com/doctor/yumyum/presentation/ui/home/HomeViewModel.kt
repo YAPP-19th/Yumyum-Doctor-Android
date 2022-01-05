1541ab9aebc09b27ed9b8a798e5df88036a1a6b6
junieberry <junyoung44@g.skku.edu>
2022-01-06 02:13:54 +0900

    [Update] 홈 화면 닉네임+레벨로 수정

--- app/src/main/java/com/doctor/yumyum/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/doctor/yumyum/presentation/ui/home/HomeViewModel.kt
@@ -1,9 +1,12 @@
 package com.doctor.yumyum.presentation.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.doctor.yumyum.R
 import com.doctor.yumyum.common.base.BaseViewModel
+import com.doctor.yumyum.common.utils.gradeEnToKo
 import com.doctor.yumyum.data.model.FavoriteRecipe
 import com.doctor.yumyum.data.remote.response.FavoriteRecipeResponse
 import com.doctor.yumyum.data.model.RecipeModel
@@ -22,14 +25,19 @@ class HomeViewModel : BaseViewModel() {
     private val _mode: MutableLiveData<Int> = MutableLiveData(mainRepository.getMode())
     val mode: LiveData<Int>
         get() = _mode
-
+    private val _nickname: MutableLiveData<String> = MutableLiveData()
+    val nickname: LiveData<String>
+        get() = _nickname
+    private val _grade: MutableLiveData<String> = MutableLiveData()
+    val grade: LiveData<String>
+        get() = _grade
+    val userInfo: MediatorLiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+        addSource(nickname) { value = isInfoReady() }
+        addSource(grade) { value = isInfoReady() }
+    }
     private val _favoriteList: MutableLiveData<ArrayList<FavoriteRecipe>> = MutableLiveData()
     val favoriteList: LiveData<ArrayList<FavoriteRecipe>>
         get() = _favoriteList
-
-    private val _nickname: MutableLiveData<String> = MutableLiveData("")
-    val nickname: LiveData<String>
-        get() = _nickname
     private val _recommendationList: MutableLiveData<ArrayList<RecipeModel>> = MutableLiveData()
     val recommendationList: LiveData<ArrayList<RecipeModel>>
         get() = _recommendationList
@@ -37,20 +45,29 @@ class HomeViewModel : BaseViewModel() {
     val errorState: LiveData<Boolean>
         get() = _errorState
 
+
     fun changeMode() {
         _mode.value =
             if (mode.value == R.string.common_food) R.string.common_beverage else R.string.common_food
         mainRepository.setMode(mode.value ?: R.string.common_food)
     }
 
-    suspend fun getUserNickname() {
+    suspend fun getUserInfo() {
         try {
             val userInfoResponse: Response<UserInfoResponse> = userRepository.getUserInfo()
-            _nickname.postValue(userInfoResponse.body()?.userInfo?.nickname)
+            if (userInfoResponse.isSuccessful) {
+                _nickname.postValue(userInfoResponse.body()?.userInfo?.nickname)
+                _grade.postValue(userInfoResponse.body()?.userInfo?.grade?.let { gradeEnToKo(it) })
+            } else {
+                _errorState.postValue(true)
+            }
         } catch (e: Exception) {
             _errorState.postValue(true)
         }
     }
+
+    private fun isInfoReady(): Boolean =
+        !nickname.value.isNullOrEmpty() && !grade.value.isNullOrEmpty()
 
     suspend fun getFavorite(categoryName: String) {
         try {
